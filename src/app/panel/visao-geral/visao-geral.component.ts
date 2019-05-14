0d0fd9e8f8c64818329f00cccc83abf972e136ce
C0balto <hewertonrodrigues@gmail.com>
2019-05-14 16:28:06 -0300

    atualizacao

---
+++ src/app/panel/visao-geral/visao-geral.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MongoService } from 'src/app/services/mongo.service';
+
+@Component({
+  selector: 'dwa-visao-geral',
+  templateUrl: './visao-geral.component.html',
+  styleUrls: ['./visao-geral.component.css']
+})
+export class VisaoGeralComponent implements OnInit {
+
+  retornoDB: any;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  // testeDB() {
+  //   return this.mongoService.getDB().subscribe((data) => {
+  //     console.log(data);
+  //     this.retornoDB = data;
+  //   });
+  // }
+}
